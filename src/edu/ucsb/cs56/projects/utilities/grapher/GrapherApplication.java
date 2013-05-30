8ebdbdf2eb47b187404fb31b4773f31713d5957e
Ryan Halbrook <ryanhalbrook@umail.ucsb.edu>
2013-05-29 21:32:40 -0700

    Added clean target and basic javadoc target

--- src/edu/ucsb/cs56/projects/utilities/grapher/GrapherApplication.java
+++ src/edu/ucsb/cs56/projects/utilities/grapher/GrapherApplication.java
@@ -1,15 +1,15 @@
 import javax.swing.JFrame;
+
+/**
+   Class to start the grapher utility
+   @author Ryan Halbrook
+   @version CS56 Spring 2013
+*/
 public class GrapherApplication {
 
     public static final String TITLE = "2D Function Grapher";
     public static final int WIDTH = 500;
     public static final int HEIGHT = 350;
-
-    /**
-       Class to start the grapher utility
-       @author Ryan Halbrook
-       @version CS56 Spring 2013
-     */
 
     public static void main(String [] args) {
 	JFrame appFrame = new JFrame(TITLE);
