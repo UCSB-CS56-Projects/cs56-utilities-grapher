03b915ef6e95f94fc3acfd9a3226aa6b05436df8
Ryan Halbrook <ryanhalbrook@umail.ucsb.edu>
2013-05-29 21:03:52 -0700

    Added a basic class to open a window and a build file with compile and run targets

---
+++ src/edu/ucsb/cs56/projects/utilities/grapher/GrapherApplication.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+public class GrapherApplication {
+
+    public static final String TITLE = "2D Function Grapher";
+    public static final int WIDTH = 500;
+    public static final int HEIGHT = 350;
+
+    /**
+       Class to start the grapher utility
+       @author Ryan Halbrook
+       @version CS56 Spring 2013
+     */
+
+    public static void main(String [] args) {
+	JFrame appFrame = new JFrame(TITLE);
+	appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	appFrame.setSize(WIDTH, HEIGHT);
+	appFrame.setVisible(true);
+    }
+
+} // End of class
