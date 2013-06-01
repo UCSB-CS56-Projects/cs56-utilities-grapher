f9ce6a243832f1c8c57a43b0398118ea757167d2
Ryan Halbrook <ryanhalbrook@umail.ucsb.edu>
2013-06-01 14:03:18 -0700

    Added a stub class for an arbitrary function

---
+++ src/edu/ucsb/cs56/projects/utilities/grapher/ArbitraryFunctionR1R1.java
@@ -0,0 +1,26 @@
+
+/**
+   A class that represents an arbitrary function that is given by a String.
+   @author Ryan Halbrook
+   @version CS56, S13, Project
+ */
+public class ArbitraryFunctionR1R1 implements FunctionR1R1 {
+
+    /**
+       Creates a new object based on the given expression.
+       @param expression the expression to evaluate the function with.
+       @param indVar the independent variable in the expression (i.e. 'x').
+     */
+    public ArbitraryFunctionR1R1(String expression, char indVar) {
+	
+    }
+
+    public double evaluate(double indVar) {
+	return -1; // STUB
+    }
+
+    public boolean isInDomain(double arg) {
+	return false; // STUB
+    }
+
+}
